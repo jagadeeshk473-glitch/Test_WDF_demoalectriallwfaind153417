39a3724c03999cf3a90d8978ea0d2df314592332
System Administrator <admin@example.com>
2026-08-15 20:27:52 +0530

    Privilege changes

---
+++ src/fluent/generated/other/sys-scope-privilege/sys_scope_privilege_8b138b102ffe4f10809dea3fafa4e328.now.ts
@@ -0,0 +1,10 @@
+import { CrossScopePrivilege } from '@servicenow/sdk/core'
+
+CrossScopePrivilege({
+    $id: Now.ID['8b138b102ffe4f10809dea3fafa4e328'],
+    operation: 'execute',
+    status: 'allowed',
+    targetName: 'ScriptableServiceResultBuilder.setStatus',
+    targetScope: 'global',
+    targetType: 'scriptable',
+})
---
+++ src/fluent/generated/other/sys-scope-privilege/sys_scope_privilege_fe1383102ffe4f10809dea3fafa4e39f.now.ts
@@ -0,0 +1,10 @@
+import { CrossScopePrivilege } from '@servicenow/sdk/core'
+
+CrossScopePrivilege({
+    $id: Now.ID['fe1383102ffe4f10809dea3fafa4e39f'],
+    operation: 'execute',
+    status: 'allowed',
+    targetName: 'RESTAPIRequest',
+    targetScope: 'global',
+    targetType: 'scriptable',
+})
